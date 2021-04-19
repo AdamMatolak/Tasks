fc6b08a013f8476d47a0902e9eaa8860b46bdc78
AdamMatolak <adam.matolak@kosickaakademia.sk>
2021-04-19 10:25:18 +0200

    Updated MongoImpl.java

--- src/main/java/sk/kosickaakademia/matolak/tasks/mongoDb/MongoImpl.java
+++ src/main/java/sk/kosickaakademia/matolak/tasks/mongoDb/MongoImpl.java
@@ -22,23 +22,6 @@ public class MongoImpl implements MongoDb {
     private static MongoCollection<Document> collection;
     private static Date date = new Date();
 
-    /*public boolean insertTask(String title, String task, int priority, double price) {
-        if(title==null || title.equals("") || task==null || task.equals("") || priority<1 || priority>3 || price<=0)
-            return false;
-        database=mongoClient.getDatabase("TasksDB");
-        collection = database.getCollection("Tasks");
-        docs=new Document("task",task);
-        docs= new Document();
-        docs.append("date", date);
-        docs.append("title", title);
-        docs.append("task", task);
-        docs.append("priority", priority);
-        docs.append("price", price);
-        docs.append("done", false);
-        collection.insertOne(docs);
-        return true;
-    }
-*/
     @Override
     public void insertTask(Task task) {
         if(task==null){
