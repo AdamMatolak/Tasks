94b49ccb5b9c63b8fe574e8dbf32f5d71b480ad7
AdamMatolak <adam.matolak@kosickaakademia.sk>
2021-04-19 09:36:01 +0200

    Updated Task.java

--- src/main/java/sk/kosickaakademia/matolak/tasks/collection/Task.java
+++ src/main/java/sk/kosickaakademia/matolak/tasks/collection/Task.java
@@ -1,13 +1,57 @@
 package sk.kosickaakademia.matolak.tasks.collection;
 
+import org.bson.types.ObjectId;
+
 import java.util.Date;
 
 public class Task {
 
-
+    private ObjectId id;
     private String name;
     private int priority;
     private boolean done;
     private Date date;
     private double price;
+
+    public Task(String name, int priority, boolean done, Date date) {
+        this.name = name;
+        this.priority = priority;
+        this.done = done;
+        this.date = date;
+        this.price = -1;
+        id = null;
+    }
+
+    public Task(String name, int priority, boolean done, Date date, double price) {
+        this(name, priority, done, date);
+        this.price = price;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
